dc1edafbbe724d4393174e8af45301c5a3d69cc3
milan.forrat <milan.forrat@gmail.com>
2021-06-01 15:10:13 +0200

    feat: wip on project

--- jeu.js
+++ jeu.js
@@ -91,152 +91,199 @@ document.onkeydown = function play(enter){
 
     // si j'appuie sur "entrer"
     if(enter.keyCode == 13){
-    document.body.className ="";
-    startTitle.style.display ="none";
-    document.onkeydown = function (event){
+        document.body.className ="";
+        startTitle.style.display ="none";
+        document.onkeydown = function (event){
 
-        // permet de voir quelle touche est appuyée
-        // console.log(event.keyCode);
-        // touche fleche du haut
-        if(event.keyCode == 38){
-            vaisseau.top -= 10;
-        }
-        // left
-        if(event.keyCode == 37){
-            vaisseau.left -= 10;
-        }
-        //right
-        if(event.keyCode == 39){
-            vaisseau.left +=  10;
-        }
-        //down
-        if(event.keyCode == 40){
-            vaisseau.top +=  10;
-        }
-        // espace = tir
-        if(event.keyCode == 32){
-            // on ne peut tirer qu'un missile à la fois
-            if(missile.display == "none"){
-                // affichage du missile
-                missile.display="block";
-                // je centre le missile au milieu du vaisseau
-                missile.left = vaisseau.left + (vaisseau._node.width - missile._node.width) /2
-                // je mets le missile au dessus du vaisseau
-                missile.top = vaisseau.top;
-                // j'invoque la fonction d'envoie du missile 
-                // param 1 = le nom de la fonction 
-                // param 2 = la fréquence de rafraichissement
-                missile.startAnimation(moveMissile, 20);
+            // permet de voir quelle touche est appuyée
+            // console.log(event.keyCode);
+            // touche fleche du haut
+            if(event.keyCode == 38){
+                vaisseau.top -= 10;
             }
-            
-        }  
+            // left
+            if(event.keyCode == 37){
+                vaisseau.left -= 10;
+            }
+            //right
+            if(event.keyCode == 39){
+                vaisseau.left +=  10;
+            }
+            //down
+            if(event.keyCode == 40){
+                vaisseau.top +=  10;
+            }
+            // espace = tir
+            if(event.keyCode == 32){
+                // on ne peut tirer qu'un missile à la fois
+                if(missile.display == "none"){
+                    // affichage du missile
+                    missile.display="block";
+                    // je centre le missile au milieu du vaisseau
+                    missile.left = vaisseau.left + (vaisseau._node.width - missile._node.width) /2
+                    // je mets le missile au dessus du vaisseau
+                    missile.top = vaisseau.top;
+                    // j'invoque la fonction d'envoie du missile 
+                    // param 1 = le nom de la fonction 
+                    // param 2 = la fréquence de rafraichissement
+                    missile.startAnimation(moveMissile, 20);
+                }
+                
+            }  
 
-    }
+        }
 
-    // -------------------------------- vérifications qu'on ne sort pas du cadre  ------------
-    if(vaisseau.left < 0){
-        // si je sors du cadre alors je suis redéplacer à la valeur 0 (bord de map)
-        vaisseau.left =0;
-    }
-    // je vérifie que je ne sors pas à droite, donc je prends en compte la largeur du client - la largeur de l'image
-    if(vaisseau.left > document.body.clientWidth - vaisseau._node.width){
-         vaisseau.left = document.body.clientWidth - vaisseau._node.width;
-    }
-    if(vaisseau.top < 0){
-        // si je sors du cadre alors je suis redéplacer à la valeur 0 (bord de map)
-        vaisseau.top =0;
-    }
-    if(vaisseau.top > document.body.clientHeight - vaisseau._node.height){
-        vaisseau.top = document.body.clientHeight - vaisseau._node.height;
-    }
-    if(window.onresize){
+        
+        // -------------------------------- vérifications qu'on ne sort pas du cadre  ------------
         if(vaisseau.left < 0){
             // si je sors du cadre alors je suis redéplacer à la valeur 0 (bord de map)
-            vaisseau.left = 0;
+            vaisseau.left =0;
         }
         // je vérifie que je ne sors pas à droite, donc je prends en compte la largeur du client - la largeur de l'image
         if(vaisseau.left > document.body.clientWidth - vaisseau._node.width){
-             vaisseau.left = document.body.clientWidth - vaisseau._node.width;
+            vaisseau.left = document.body.clientWidth - vaisseau._node.width;
         }
         if(vaisseau.top < 0){
             // si je sors du cadre alors je suis redéplacer à la valeur 0 (bord de map)
-            vaisseau.top = 0;
+            vaisseau.top =0;
         }
         if(vaisseau.top > document.body.clientHeight - vaisseau._node.height){
             vaisseau.top = document.body.clientHeight - vaisseau._node.height;
         }
-    }
-    // ----------------------      création de l'interval de déplacement du missile et des monstres    ---------------------------------
-    
-    // on ajoute une animation de départ
-    // paramètre 1 = le nom de la fonction à rafraichir/rééxécuter (ex: startAnimation(missile, 20))
-    // paramètre 2 = l'interval (le délais de rééxécution de l'action)
-    Sprite.prototype.startAnimation = function (fct, interval) {
-        // s'il existe déjà un interval (ex: un missile tiré), alors je l'annule
-        if(this._clock) {
+        if(window.onresize){
+            if(vaisseau.left < 0){
+                // si je sors du cadre alors je suis redéplacer à la valeur 0 (bord de map)
+                vaisseau.left = 0;
+            }
+            // je vérifie que je ne sors pas à droite, donc je prends en compte la largeur du client - la largeur de l'image
+            if(vaisseau.left > document.body.clientWidth - vaisseau._node.width){
+                vaisseau.left = document.body.clientWidth - vaisseau._node.width;
+            }
+            if(vaisseau.top < 0){
+                // si je sors du cadre alors je suis redéplacer à la valeur 0 (bord de map)
+                vaisseau.top = 0;
+            }
+            if(vaisseau.top > document.body.clientHeight - vaisseau._node.height){
+                vaisseau.top = document.body.clientHeight - vaisseau._node.height;
+            }
+        }
+        // ----------------------      création de l'interval de déplacement du missile et des monstres    ---------------------------------
+        
+        // on ajoute une animation de départ
+        // paramètre 1 = le nom de la fonction à rafraichir/rééxécuter (ex: startAnimation(missile, 20))
+        // paramètre 2 = l'interval (le délais de rééxécution de l'action)
+        Sprite.prototype.startAnimation = function (fct, interval) {
+            // s'il existe déjà un interval (ex: un missile tiré), alors je l'annule
+            if(this._clock) {
+                window.clearInterval(this._clock);
+            }
+            // on associe à _this la valeur actuelle de l'objet
+            let _this = this;
+            // je déclenche à interval régulier et je stock cette action dans une variable _clock
+            this._clock = window.setInterval(function() {
+                // le _this correspond à l'élément et non pas à la fonction grâce à la ligne 140
+                fct (_this);
+            }, interval);
+        };
+        // on arrête l'animation de départ stockée dans la variable _clock
+        Sprite.prototype.stopAnimation = function () {
             window.clearInterval(this._clock);
-        }
-        // on associe à _this la valeur actuelle de l'objet
-        let _this = this;
-        // je déclenche à interval régulier et je stock cette action dans une variable _clock
-        this._clock = window.setInterval(function() {
-            // le _this correspond à l'élément et non pas à la fonction grâce à la ligne 140
-            fct (_this);
-        }, interval);
-    };
-    // on arrête l'animation de départ stockée dans la variable _clock
-    Sprite.prototype.stopAnimation = function () {
-        window.clearInterval(this._clock);
-    }
+        };
 
-    // ---------------------- Création du déplacement du missile et des monstres ---------------------------------
+        // ----------------------- gestion de la colision -------------
+        //check collision doit me retourner s'il y a une collision
+        Sprite.prototype.checkCollision = function (other) {
+            // je souhaite l'inverse du test ci dessous, donc je regarde s'il y a collision
+            return ! 
+                // je test qu'il n'y ait pas de collision
+            (
 
-    function moveMissile(missile){
-        // je fais monter le missile (la valeur correspond à la vitesse du missile)
-        missile.top -= 10;
-        // arrête le missile lorsqu'il sort de sa fenêtre
-        if(missile.top < -100) {
-            missile.stopAnimation();
-            missile.display = "none";
+                // si je me trouve en dessous de l'élement = pas de collision
+                (this.top + this._node.height < other.top) 
+                                    ||
+                // si je me trouve au dessus de l'élément = pas de collision
+                this.top > (other.top + other._node.height)
+                                    ||
+                // si je ne touche pas à droite = pas de collision
+                (this.left + this._node.width < other.left)
+                                    ||
+                // si je bne touche pas à gauche = pas de collision
+                this.left > (other.left + other._node.width)
+            );
         }
-    }
 
-    function moveMonsterToRight(monster){
-        // la valeur est la vitesse de déplacement vers la droite
-        monster.left += 20;
-        // si le monstre arrive au bord de map
-        // je déduis la largeur de l'image du monstre pour ne pas soritr du cadre
-        if(monster.left > document.body.clientWidth - monster._node.width){
-            // je vais descendre mon monstre quand il arrive en bout de ligne
-            monster.top += 30;
-            // j'empêche mon monstre de sortir du cadre
-            monster.left = document.body.clientWidth - monster._node.width;
-            // je déclenche l'animation du monstre avec la fonction toRight, rafraichie à 20ms
-            monster.startAnimation(moveMonsterToLeft, 20);
-        }
-        // si le monstre dépasse j'arrête son animation 
-        if(monster.top > document.body.clientHeight + monster._node.height) {
-            monster.stopAnimation();
-        }
-    }
+        // ---------------------- Création du déplacement du missile et des monstres ---------------------------------
 
-    function moveMonsterToLeft(monster){
-        // la valeur est la vitesse de déplacement vers la droite
-        monster.left -= 20;
-        // si le monstre arrive au bord de map
-        // je déduis la largeur de l'image du monstre pour ne pas soritr du cadre
-        if(monster.left <= 0){
-            // je vais descendre mon monstre quand il arrive en bout de ligne
-            monster.top += 30;
-            // je déclenche l'animation du monstre avec la fonction toRight, rafraichie à 20ms
-            monster.startAnimation(moveMonsterToRight, 20);
+        function moveMissile(missile){
+            // je fais monter le missile (la valeur correspond à la vitesse du missile)
+            missile.top -= 10;
+            // arrête le missile lorsqu'il sort de sa fenêtre
+            if(missile.top < -100) {
+                missile.stopAnimation();
+                missile.display = "none";
+            }
+            // si je tire sur un élément invisible alors je continue
+            if(m1.display == "none"){
+                return;
+            }
+            if(missile.checkCollision(m1)){
+                missile.stopAnimation();
+                m1.stopAnimation();
+                // si je touche un monstre alors je cache le missile
+                missile.display ="none";
+                // je cache le monstre
+                m1.display ="none";
+            }
         }
-        // si le monstre dépasse j'arrête son animation
-        if(monster.top > document.body.clientHeight + monster._node.height) {
-            monster.stopAnimation();
+
+        function moveMonsterToRight(monster){
+            // la valeur est la vitesse de déplacement vers la droite
+            monster.left += 20;
+            // si le monstre arrive au bord de map
+            // je déduis la largeur de l'image du monstre pour ne pas soritr du cadre
+            if(monster.left > document.body.clientWidth - monster._node.width){
+                // je vais descendre mon monstre quand il arrive en bout de ligne
+                monster.top += 30;
+                // j'empêche mon monstre de sortir du cadre
+                monster.left = document.body.clientWidth - monster._node.width;
+                // je déclenche l'animation du monstre avec la fonction toRight, rafraichie à 20ms
+                monster.startAnimation(moveMonsterToLeft, 20);
+            }
+            // si le monstre dépasse j'arrête son animation 
+            if(monster.top > document.body.clientHeight + monster._node.height) {
+                monster.stopAnimation();
+            }
         }
+
+        function moveMonsterToLeft(monster){
+            // la valeur est la vitesse de déplacement vers la droite
+            monster.left -= 20;
+            // si le monstre arrive au bord de map
+            // je déduis la largeur de l'image du monstre pour ne pas soritr du cadre
+            if(monster.left <= 0){
+                // je vais descendre mon monstre quand il arrive en bout de ligne
+                monster.top += 30;
+                // je déclenche l'animation du monstre avec la fonction toRight, rafraichie à 20ms
+                monster.startAnimation(moveMonsterToRight, 20);
+            }
+            // si le monstre dépasse j'arrête son animation
+            if(monster.top > document.body.clientHeight + monster._node.height) {
+                monster.stopAnimation();
+            }
+        }
+        // m2.startAnimation(moveMonsterToRight, 20);
+         m1.startAnimation(moveMonsterToRight, 20);
+        // boucle qui lance les monstres 
+        // for(var i = 1; i < 6 ;i++){
+        //     element = "m"+i;
+        //     console.log(element);
+        //     element.startAnimation(moveMonsterToRight, 20);
+        // }
+
+
+
+
+    //fin du if 
     }
-    
-    m1.startAnimation(moveMonsterToRight, 20);
-    }
+// fin de la fonction    
 }
